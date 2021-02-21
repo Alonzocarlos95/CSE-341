d5f5619e3ff1409dbfb041729963dab8e766ae43
Carlos Alonzo <carlosalonzo95@hotmail.com>
2021-02-21 00:37:39 -0500

    Update backend assignments

--- web/backend_assignments/week2/index.php
+++ web/backend_assignments/week2/index.php
@@ -7,7 +7,7 @@ if ($action == NULL){
 
 switch($action){
     case 'shopping-cart':
-        include '../../shopping_cart/';
+        include '../../shopping_cart/index.php';
         break;
         default:
         include '../index.php';
